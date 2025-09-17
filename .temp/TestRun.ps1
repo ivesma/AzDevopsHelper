2eca7e5ddd18a7d2df2f404ec7f5d5213ce022ee
ivesma <ivesma@gmail.com>
2025-09-17 16:04:55 +0100

    Update .gitignore to include all .temp files and remove TestRun.ps1 script

--- .temp/TestRun.ps1
+++
@@ -1,8 +0,0 @@
-#Install-Module -Name powershell-yaml -Scope CurrentUser -Force
-Import-Module (Join-Path $PSScriptRoot '../AzDevopsHelper.psd1' -Resolve) -Force -Verbose
-
-Get-Command -Module powershell-yaml
-
-$cred = New-LocalCredential -AppName 'AzDO_Service_Access' -OSType 'Windows'
-$pat = $cred.GetNetworkCredential().Password
-$pat
